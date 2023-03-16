69b0862cd6488c60ffdfe6d3620779e21ff50ae9
Peter Bomber <peter.bomber@microsoft.com>
2023-03-17 11:09:53 +1300

    declare a winner

--- frameworks/hello-world-react-vite/webview-ui/src/Board.tsx
+++ frameworks/hello-world-react-vite/webview-ui/src/Board.tsx
@@ -6,6 +6,7 @@ interface BoardProps {}
 interface BoardState {
   squares: (string | null)[]
   xIsNext: boolean
+  winner: string | null
 }
 
 export class Board extends React.Component<BoardProps, BoardState> {
@@ -13,17 +14,23 @@ export class Board extends React.Component<BoardProps, BoardState> {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true
+      xIsNext: true,
+      winner: null
     }
   }
 
   handleClick(i: number) {
+    if (this.state.winner) {
+      return;
+    }
+
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares,
-      xIsNext: !this.state.xIsNext}
-    );
+      xIsNext: !this.state.xIsNext,
+      winner: calculateWinner(squares)
+    });
   }
 
   renderSquare(i: number) {
@@ -36,7 +43,7 @@ export class Board extends React.Component<BoardProps, BoardState> {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const status = this.state.winner ? `Winner: ${this.state.winner}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
@@ -60,4 +67,23 @@ export class Board extends React.Component<BoardProps, BoardState> {
     );
   }
 }
-  
+
+function calculateWinner(squares: (string | null)[]): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  const line = lines.find(l => {
+    const [a, b, c] = l;
+    return squares[a] && squares[a] === squares[b] && squares[b] === squares[c];
+  });
+
+  return line && squares[line[0]] || null;
+}
