893fa31e190bd3e8e778b7e2d1b4eb99120a8846
Peter Bomber <peter.bomber@microsoft.com>
2023-03-17 12:09:17 +1300

    move board state to game and keep history

--- frameworks/hello-world-react-vite/webview-ui/src/Board.tsx
+++ frameworks/hello-world-react-vite/webview-ui/src/Board.tsx
@@ -1,53 +1,29 @@
 import React from "react";
 import { Square } from "./Square";
 
-interface BoardProps {}
+export type SquareValue = string | null;
+export type SquareValues = SquareValue[];
 
-interface BoardState {
-  squares: (string | null)[]
-  xIsNext: boolean
-  winner: string | null
+interface BoardProps {
+  squares: SquareValues
+  onSquareClick: (index: number) => void
 }
 
+interface BoardState {}
+
 export class Board extends React.Component<BoardProps, BoardState> {
-  constructor(props: BoardProps) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-      winner: null
-    }
-  }
-
-  handleClick(i: number) {
-    if (this.state.winner) {
-      return;
-    }
-
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares,
-      xIsNext: !this.state.xIsNext,
-      winner: calculateWinner(squares)
-    });
-  }
-
   renderSquare(i: number) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onSquareClick(i)}
       />
     );
   }
 
   render() {
-    const status = this.state.winner ? `Winner: ${this.state.winner}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -66,24 +42,4 @@ export class Board extends React.Component<BoardProps, BoardState> {
       </div>
     );
   }
-}
-
-function calculateWinner(squares: (string | null)[]): string | null {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  const line = lines.find(l => {
-    const [a, b, c] = l;
-    return squares[a] && squares[a] === squares[b] && squares[b] === squares[c];
-  });
-
-  return line && squares[line[0]] || null;
 }
--- frameworks/hello-world-react-vite/webview-ui/src/Game.tsx
+++ frameworks/hello-world-react-vite/webview-ui/src/Game.tsx
@@ -1,20 +1,85 @@
 import React from "react";
-import { Board } from "./Board";
+import { Board, SquareValues } from "./Board";
 import './Game.css';
 
-export class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+interface GameProps {}
+
+interface GameState {
+  history: SquareValues[]
+  turnIndex: number
+  winner: string | null
+}
+
+export class Game extends React.Component<GameProps, GameState> {
+  constructor(props: GameProps) {
+    super(props);
+    this.state = {
+      history: [
+        Array(9).fill(null)
+      ],
+      turnIndex: 0,
+      winner: null
     }
   }
-  
+
+  getNextPlayer() {
+    return this.state.turnIndex % 2 === 0 ? 'X' : 'O';
+  }
+
+  handleSquareClick(i: number) {
+    if (this.state.winner) {
+      return;
+    }
+
+    const turnSquares = this.state.history[this.state.turnIndex].slice();
+    if (turnSquares[i]) {
+      return;
+    }
+
+    turnSquares[i] = this.getNextPlayer();
+    const previousHistory = this.state.history.slice(0, this.state.turnIndex + 1);
+
+    this.setState({
+      history: [...previousHistory, turnSquares],
+      turnIndex: this.state.turnIndex + 1,
+      winner: calculateWinner(turnSquares)
+    });
+  }
+
+  render() {
+    const turnSquares = this.state.history[this.state.turnIndex];
+    const status = this.state.winner ? `Winner: ${this.state.winner}` : `Next player: ${this.getNextPlayer()}`;
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={turnSquares} onSquareClick={i => this.handleSquareClick(i)} />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares: SquareValues): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  const line = lines.find(l => {
+    const [a, b, c] = l;
+    return squares[a] && squares[a] === squares[b] && squares[b] === squares[c];
+  });
+
+  return line && squares[line[0]] || null;
+}
